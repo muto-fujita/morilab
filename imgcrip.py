0f51fc71afbaf7087f2764044d5eee6e5e849db2
takeshi30rou <takeshi30rou@gmail.com>
2019-12-21 19:27:46 +0900

    Update imgcrip.py
    座標を割合で指定

--- imgcrip.py
+++ imgcrip.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def get4PlatesFromAImage(filename, x, y, width, height):
     """
@@ -9,16 +10,29 @@ def get4PlatesFromAImage(filename, x, y, width, height):
     ----------
     filename : char
         画像の名前
-    x : int x[4]
+    x : ndarray x[4]
         プレートのX座標
-    y : int y[4]
+    y : ndarray y[4]
         プレートのy座標
-    width : int width[4]
+    width : ndarray width[4]
         プレートのwidth座標
-    height : int height[4]
+    height : ndarray height[4]
         プレートのheight座標
     """
     img = cv2.imread(filename)
+    # カラーとグレースケールで場合分け
+    if len(img.shape) == 3:
+        img_height, img_width, channels = img.shape[:3]
+    else:
+        img_height, img_width = img.shape[:2]
+        channels = 1
+
+    # 切り出し座標を取得
+    x = np.array(x * img_width, dtype=np.int32)
+    y = np.array(y * img_height, dtype=np.int32)
+    width = np.array(width * img_width, dtype=np.int32)
+    height = np.array(height * img_height, dtype=np.int32)
+
     for i in range(4):
         plate = img[y[i]:y[i]+height[i],x[i]:x[i]+width[i]]
         plate= cv2.rotate(plate, cv2.ROTATE_90_COUNTERCLOCKWISE) #反時計回りに９０度回転
@@ -26,9 +40,10 @@ def get4PlatesFromAImage(filename, x, y, width, height):
         cv2.imwrite(plateName, plate)
 
 if __name__ == '__main__':
-    x = [204, 204, 1823, 1824]
-    y = [47, 2063, 42, 2063]
-    width = [1302, 1302, 3129, 3122]
-    height = [1945, 3961, 1940, 3961]
+    # 座標を割合で指定
+    x = np.array([204, 204, 1823, 1824])/3200
+    y = np.array([47, 2063, 42, 2063])/4000
+    width = np.array([1302, 1302, 3129, 3122])/3200
+    height = np.array([1945, 3961, 1940, 3961])/4000
 
     get4PlatesFromAImage("./scanimg.jpg", x, y, width, height)
